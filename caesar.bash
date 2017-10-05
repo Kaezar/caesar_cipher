f121aa80c32cca918b75da002345de3b9e90e2eb
Kaezar <rokki9@gmail.com>
2017-10-05 01:02:44 -0500

    improved caesar.bash

--- caesar.bash
+++ caesar.bash
@@ -1,2 +1,7 @@
 #!/bin/bash
+gcc filterA.c -o filterA
+gcc filterB.c -o filterB
+gcc filterC.c -o filterC
+gcc filterD.c -o filterD
+gcc filterE.c -o filterE
 cat $1 | ./filterA | ./filterB | ./filterC | ./filterD | ./filterE
